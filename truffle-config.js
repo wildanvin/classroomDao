a77e26d18aed3c369d8a970e4faafbfe1771ce37
wildanvin <danvin.jim@gmail.com>
2022-11-10 23:42:41 -0500

    Add Goerli network

--- truffle-config.js
+++ truffle-config.js
@@ -18,6 +18,18 @@ module.exports = {
       port: 8545, // Standard Ethereum port (default: none)
       network_id: '*', // Any network (default: none)
     },
+    goerli: {
+      provider: () =>
+        new HDWalletProvider(
+          process.env.MNEMONIC,
+          `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`
+        ),
+      network_id: 5, // Ropsten's id
+      gas: 5500000, // Ropsten has a lower block limit than mainnet
+      confirmations: 2, // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
+    },
     // Another network with more advanced options...
     // advanced: {
     // port: 8777,             // Custom port
